7916fde0ae41ce6a49f8f9846c846a5ffeba8378
Cloudstorm <mail@cloudstorm.me>
2015-04-07 21:00:44 +0200

    Removed logging to external server Code formating Removed logger

--- src/de/sabbertran/playertabcomplete/Events.java
+++ src/de/sabbertran/playertabcomplete/Events.java
@@ -5,31 +5,25 @@ import net.md_5.bungee.api.event.TabCompleteEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
-public class Events implements Listener
-{
+public class Events implements Listener {
 
     private PlayerTabComplete main;
 
-    public Events(PlayerTabComplete main)
-    {
+    public Events(PlayerTabComplete main) {
         this.main = main;
     }
 
     @EventHandler
-    public void onTabComplete(TabCompleteEvent ev)
-    {
+    public void onTabComplete(TabCompleteEvent ev) {
         String partialPlayerName = ev.getCursor().toLowerCase();
 
         int lastSpaceIndex = partialPlayerName.lastIndexOf(' ');
-        if (lastSpaceIndex >= 0)
-        {
+        if (lastSpaceIndex >= 0) {
             partialPlayerName = partialPlayerName.substring(lastSpaceIndex + 1);
         }
 
-        for (ProxiedPlayer p : main.getProxy().getPlayers())
-        {
-            if (p.getName().toLowerCase().startsWith(partialPlayerName))
-            {
+        for (ProxiedPlayer p : main.getProxy().getPlayers()) {
+            if (p.getName().toLowerCase().startsWith(partialPlayerName)) {
                 ev.getSuggestions().add(p.getName());
             }
         }
